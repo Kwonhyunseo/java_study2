61d6e2117aea00996bb808740ec5ecf4fa25d6e8
SIST <SIST@DESKTOP-N2V7FMC>
2023-04-13 10:38:42 +0900

    1111

---
+++ Chpater5_객체지향프로그램_1/src/문제_1.java
@@ -0,0 +1,93 @@
+class Board{
+
+String poster;
+String movieNm;
+String movieEn;
+String regadte;
+String genre;
+double score;
+String nation;
+String grade;
+String time;
+int boxoffice;// 수시변경 
+String story;
+	
+    
+}
+
+
+class 영화뉴스 
+{
+	String poster;
+	String title;
+	String content;
+	String regdate;
+	String author;
+}
+
+class 카테고리{
+	String poster;
+	String title;
+	String subject;
+	
+}
+
+class 레시피
+{
+	String poster;
+	String title;
+	String chef;
+	String chef_image;
+	int star;
+	int hit;
+}
+
+
+class 멜론{
+	
+	int rank;
+	String state;
+	String poster;
+	String title;
+	String singer;
+	String album;
+	int like;
+	
+	
+}
+
+class 만개의스토어{
+	int sale;// 25  "↓"  +sale +"%"   
+	String poster;
+	String title;
+	int price; 
+	
+	
+}
+
+class 연극
+{
+	String poster;
+	String title;
+	boolean isShow;
+	String genre;
+	String regdate;
+	String loc;
+	String actor;
+	String grade;
+	String time;
+}
+
+
+public class 문제_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		
+	
+		
+	}
+
+}
